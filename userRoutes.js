9a549855d2366489bc30f1cef5b9edf9e580f6c7
Parzival2711 <89346495+Parzival2711@users.noreply.github.com>
2022-12-19 23:00:23 +0530

    Delete userRoutes.js

--- userRoutes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-    var userHandlers = require('../controllers/userController.js');
-    app.route('/auth/register').post(userHandlers.register);
-    app.route('/auth/sign_in').post(userHandlers.sign_in);
-    app.route('/profile').post(userHandlers.loginRequired,userHandlers.get_profile);
-};
-
